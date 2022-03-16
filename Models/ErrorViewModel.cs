51d14d978fb7ccfe13e01a75b6d40f9c106beca1
harisch731 <harisch731@gmail.com>
2022-03-16 11:30:53 -0700

    Created

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Pipeline_jharish.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
